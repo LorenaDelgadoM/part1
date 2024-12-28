1fc4205cd0ac44866063aef4988b4e828ffd985f
LORENA ALEJANDRA DELGADO MARTÍNEZ <lorena.delgado@senado.gob.mx>
2024-12-27 19:00:47 -0600

    feat: avance ejercicio 1.1

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+const Header = () => {
+   const course = 'Half Stack application development'
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+
+    )
+
+}
+
+const Content = (parts) => {
+
+   return (
+    <div>
+      <p>
+      {parts.part} {parts.exercise}
+      </p>
+      
+    </div>
+   ) 
+}
+
+const Total = (sum) => {
+
+  return (
+    <div>
+        <p>Number of exercises {sum.total}</p>
+    </div>
+  )
+
+
+}
+
+const App = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <>
+      <Header/>
+      <Content part={part1} exercise ={exercises1}/>
+      <Content part={part2} exercise ={exercises2}/>
+      <Content part={part3} exercise ={exercises3}/>
+      <Total total={exercises1+exercises2+exercises3}/>
+    </>
+  )
+}
+
+export default App
