c4340fea3e0cd895c294d54c876fe9171005c2a5
LORENA ALEJANDRA DELGADO MARTÍNEZ <lorena.delgado@senado.gob.mx>
2025-01-03 12:13:23 -0600

    feat: ejercicio 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,76 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+const Header = (course) => {
+   return (
+      <div>
+        <h1>{course.title}</h1>
+      </div>
+
+    )
+
+}
+
+const Part = (parts) => {
+    
+  return (
+    <div>
+      <p>
+      
+      {parts.part} {parts.exercise}
+      </p>
+      
+    </div>
+   ) 
+
+
+
+}
+
+const Content = (parts) => {
+  console.log(parts.courses[0].name)
+ 
+  return (
+    <div>
+      <Part part={parts.courses[0].name} exercise ={parts.courses[0].exercises}/>
+      <Part part={parts.courses[1].name} exercise ={parts.courses[1].exercises}/>
+      <Part part={parts.courses[2].name} exercise ={parts.courses[2].exercises}/>
+    </div>
+  )
+ 
+}
+
+const Total = (sum) => {
 
   return (
-  
-      <div>
-        <h1>{course}</h1>
-        <p>{part1.name} {part1.exercises}</p>
-        <p>{part2.name} {part2.exercises}</p>
-        <p>{part3.name} {part3.exercises}</p>
-        <p>Number of exercises {part1.exercises + part2.exercises +part3.exercises}</p>
-      </div>
-  
-      
+    <div>
+        <p>Number of exercises {sum.total}</p>
+    </div>
+  )
+
+
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    <>
+      <Header title={course} />
+      <Content courses ={parts}/>
+      <Total total= {parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+    </>
   )
 }
+
 export default App
