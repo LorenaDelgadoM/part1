1f1777df956a764caa21f651b49f7746c8c6810f
LORENA ALEJANDRA DELGADO MARTÍNEZ <lorena.delgado@senado.gob.mx>
2025-01-03 12:17:29 -0600

    feat: ejercicio 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -49,26 +49,28 @@ const Total = (sum) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
-      <Header title={course} />
-      <Content courses ={parts}/>
-      <Total total= {parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header title={course.name} />
+      <Content courses ={course.parts}/>
+      <Total total= {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </>
   )
 }
